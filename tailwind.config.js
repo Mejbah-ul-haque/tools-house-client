56aa8c4c8898ca78c3d847464cf64ea35a086a57
Mejbah <mejba.s@gmail.com>
2022-05-23 01:32:20 +0600

    daisyui installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,20 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#003366",
+          secondary: "#ff9900",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
   plugins: [require("daisyui")],
 }
