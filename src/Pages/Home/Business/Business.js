2873dd2347694ebee4ccda883e21d89e144e13e8
Mejbah <mejba.s@gmail.com>
2022-05-23 23:43:54 +0600

    added icon at business summery sections

--- src/Pages/Home/Business/Business.js
+++ src/Pages/Home/Business/Business.js
@@ -1,34 +1,35 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTools, faComment, faPeoplePulling, faDollar } from "@fortawesome/free-solid-svg-icons";
 
 const Business = () => {
-  return (
-    <div class="stats stats-vertical lg:stats-horizontal shadow">
-  
-  <div class="stat">
-    <div class="stat-title">we have</div>
-    <div class="stat-value">1000+</div>
-    <div class="stat-desc">customers</div>
-  </div>
-  <div class="stat">
-    <div class="stat-title">Downloads</div>
-    <div class="stat-value">31K</div>
-    <div class="stat-desc">Reviews</div>
-  </div>
-  
-  <div class="stat">
-    <div class="stat-title">Anual Revenue</div>
-    <div class="stat-value">120M</div>
-    <div class="stat-desc">↗︎ 400 (22%)</div>
-  </div>
-  
-  <div class="stat">
-    <div class="stat-title">New Registers</div>
-    <div class="stat-value">50+</div>
-    <div class="stat-desc">Tools</div>
-  </div>
-  
-</div>
-  );
+	return (
+		<div class="stats stats-vertical lg:stats-horizontal shadow">
+			<div class="stat">
+      <div class="stat-title text-3xl mb-5"><FontAwesomeIcon icon={faPeoplePulling}></FontAwesomeIcon></div>
+				<div class="stat-value">1000+</div>
+				<div class="stat-desc">customers</div>
+			</div>
+			<div class="stat">
+      <div class="stat-title text-3xl mb-5"><FontAwesomeIcon icon={faComment}></FontAwesomeIcon></div>
+				<div class="stat-value">31K</div>
+				<div class="stat-desc">Reviews</div>
+			</div>
+
+			<div class="stat">
+      <div class="stat-title text-3xl mb-5"><FontAwesomeIcon icon={faDollar}></FontAwesomeIcon></div>
+				<div class="stat-value">120M</div>
+				<div class="stat-desc">Anual Revenue</div>
+			</div>
+
+			<div class="stat">
+				<div class="stat-title text-3xl mb-5"><FontAwesomeIcon icon={faTools}></FontAwesomeIcon></div>
+				<div class="stat-value">50+</div>
+				<div class="stat-desc">Tools</div>
+			</div>
+      
+		</div>
+	);
 };
 
 export default Business;
