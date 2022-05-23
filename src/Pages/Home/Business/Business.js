bcba4f4f40e5849d97fab6e0d48c007f9ed1e8ee
Mejbah <mejba.s@gmail.com>
2022-05-23 23:17:52 +0600

    Business summery section updated

--- src/Pages/Home/Business/Business.js
+++ src/Pages/Home/Business/Business.js
@@ -5,21 +5,26 @@ const Business = () => {
     <div class="stats stats-vertical lg:stats-horizontal shadow">
   
   <div class="stat">
+    <div class="stat-title">we have</div>
+    <div class="stat-value">1000+</div>
+    <div class="stat-desc">customers</div>
+  </div>
+  <div class="stat">
     <div class="stat-title">Downloads</div>
     <div class="stat-value">31K</div>
-    <div class="stat-desc">Jan 1st - Feb 1st</div>
+    <div class="stat-desc">Reviews</div>
   </div>
   
   <div class="stat">
-    <div class="stat-title">New Users</div>
-    <div class="stat-value">4,200</div>
+    <div class="stat-title">Anual Revenue</div>
+    <div class="stat-value">120M</div>
     <div class="stat-desc">↗︎ 400 (22%)</div>
   </div>
   
   <div class="stat">
     <div class="stat-title">New Registers</div>
-    <div class="stat-value">1,200</div>
-    <div class="stat-desc">↘︎ 90 (14%)</div>
+    <div class="stat-value">50+</div>
+    <div class="stat-desc">Tools</div>
   </div>
   
 </div>
