54a174240ec3e9b0939950cc4c1c778b622f6727
Mejbah <mejba.s@gmail.com>
2022-05-24 16:08:21 +0600

    env.local file added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA9O6W_wNgf0irilzg9ujX6wdRpB6DNe5U",
-  authDomain: "tools-house-155bc.firebaseapp.com",
-  projectId: "tools-house-155bc",
-  storageBucket: "tools-house-155bc.appspot.com",
-  messagingSenderId: "965263764252",
-  appId: "1:965263764252:web:75c7dce153d2e0be5328d9"
+  apiKey:process.env.REACT_APP_APIKEY,
+  authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+  projectId:process.env.REACT_APP_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
