12f35c5b2224236af9b4205e08758c53fd254fee
Mejbah <mejba.s@gmail.com>
2022-05-28 07:33:02 +0600

    RequireAdmin hooks added

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import Blogs from './Pages/Blogs/Blogs';
 import BlogDetail from './Pages/BlogDetail/BlogDetail';
 import MyProfile from './Pages/Dashboard/MyProfile';
 import Users from './Pages/Dashboard/Users';
+import RequireAdmin from './Pages/Login/RequireAdmin';
 
 
 
@@ -36,7 +37,7 @@ function App() {
           <Route index element={<MyOrders></MyOrders>}></Route>
           <Route path="review" element={<MyReview></MyReview>}></Route>
           <Route path="profile" element={<MyProfile></MyProfile>}></Route>
-          <Route path="users" element={<Users></Users>}></Route>
+          <Route path="users" element={<RequireAdmin><Users></Users></RequireAdmin>}></Route>
         </Route>
         
         <Route path='login' element={<Login></Login>}></Route>
