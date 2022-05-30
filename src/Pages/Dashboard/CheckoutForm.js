8ebf19bcf4463acbe45768db057baaac6dea427b
Mejbah <mejba.s@gmail.com>
2022-05-30 07:30:02 +0600

    stripe env key

--- src/Pages/Dashboard/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -1,45 +1,62 @@
-import React from "react";
-import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
+import React, { useState } from "react";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
 const CheckoutForm = () => {
-  const stripe = useStripe();
-  const elements = useElements();
-  
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    
-    if(!stripe || !elements) {
-      return;
-    }
-    
-    const card = elements.getElement(CardElement);
-    
-    if(card === null) {
-      return;
-    }
-  }
+	const stripe = useStripe();
+	const elements = useElements();
+	const [cardError, setCardError] = useState("");
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+
+		if (!stripe || !elements) {
+			return;
+		}
+
+		const card = elements.getElement(CardElement);
+
+		if (card === null) {
+			return;
+		}
+
+		const { error, paymentMethod } = await stripe.createPaymentMethod({
+			type: "card",
+			card,
+		});
+
+		setCardError(error?.message || "");
+	};
 	return (
-		<form onSubmit={handleSubmit}>
-			<CardElement
-				options={{
-					style: {
-						base: {
-							fontSize: "16px",
-							color: "#424770",
-							"::placeholder": {
-								color: "#aab7c4",
+		<>
+			<form onSubmit={handleSubmit}>
+				<CardElement
+					options={{
+						style: {
+							base: {
+								fontSize: "16px",
+								color: "#424770",
+								"::placeholder": {
+									color: "#aab7c4",
+								},
+							},
+							invalid: {
+								color: "#9e2146",
 							},
 						},
-						invalid: {
-							color: "#9e2146",
-						},
-					},
-				}}
-			/>
-			<button className="btn btn-success btn-sm mt-5" type="submit" disabled={!stripe}>
-				Pay
-			</button>
-		</form>
+					}}
+				/>
+				<button
+					className="btn btn-success btn-sm mt-5"
+					type="submit"
+					disabled={!stripe}
+				>
+					Pay
+				</button>
+			</form>
+      {
+        cardError && <p className="text-red-500">{cardError}</p>
+      }
+		</>
 	);
 };
 
